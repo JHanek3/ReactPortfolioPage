7321d6b18eb2437a05f7d57f9088067001c7e565
Jon <>
2020-10-07 16:49:28 -0500

    Fixed chatapp for localhost4000

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const http = require('http')
+const app = module.exports.app = express()
+
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+const PORT = process.env.PORT || 4000;
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+
+io.on("connection", (socket) => {
+  console.log(`Client ${socket.id} connected`);
+
+  // Join a conversation
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  // Leave the room if the user closes the socket
+  socket.on("disconnect", () => {
+    console.log(`Client ${socket.id} diconnected`);
+    socket.leave(roomId);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
